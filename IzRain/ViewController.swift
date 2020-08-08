5bc70d7fda4718e1859ed7bc8ee070e0f8653085
izzatjabali <izyjab@gmail.com>
2020-08-08 15:57:07 +1000

    Initial Commit

---
+++ IzRain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IzRain
+//
+//  Created by Izzat Jabali on 8/8/20.
+//  Copyright © 2020 Izzat Jabali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

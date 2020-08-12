a92756af2f3098cdd5c4e57c2bb744db57f02e05
izzatjabali <izyjab@gmail.com>
2020-08-12 19:55:07 +1000

    Everything on screen has constraints  added. Vector images all added correctly. The application successfully builds and runs but does not fetch the correct data.

--- IzRain/Model/WeatherData.swift
+++ IzRain/Model/WeatherData.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Izzat Jabali. All rights reserved.
 //
 
+//
+
 import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+   
+}
+
+struct Main: Codable {
+    let temp: Double
+   
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
